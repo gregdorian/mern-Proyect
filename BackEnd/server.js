59e6ac2eb680ef037f0b3477202a629eca4405d3
gregdorian <greg_dorian@yahoo.com>
2023-10-13 10:31:55 -0500

    back and front in 2 directories

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-// const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
@@ -27,6 +26,8 @@ app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.use('/', require('./routes/root'))
+app.use('/users', require('./routes/userRoutes'))
+app.use('/notes', require('./routes/noteRoutes'))
 
  app.all('*', (req, res) => {
     res.status(404)
@@ -40,6 +41,7 @@ app.use('/', require('./routes/root'))
 })
 
 app.use(errorHandler)
+
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB')
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
