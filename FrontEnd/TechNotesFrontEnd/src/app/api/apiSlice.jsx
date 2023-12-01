c6267932287a34ddd69a8248d435019b5d56ac14
gregdorian <greg_dorian@yahoo.com>
2023-12-01 09:41:38 -0500

    apislice changes

--- FrontEnd/TechNotesFrontEnd/src/app/api/apiSlice.jsx
+++ FrontEnd/TechNotesFrontEnd/src/app/api/apiSlice.jsx
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    //baseUrl: 'http://localhost:3500',
-    baseUrl: 'https://timel.gregdorian.repl.co/',//***for use via repl.it */
+    baseUrl: 'http://localhost:3500',
+    //baseUrl: 'https://timel.gregdorian.repl.co/',//***for use via repl.it */
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
