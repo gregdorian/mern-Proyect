f2dfa501479fec39e7b45f656bec81e988b50f6a
german L. R <greg_dorian@yahoo.com>
2023-12-12 16:07:25 -0500

    comentarie el http del apiallowedOrigins.js
    se coloca solo el url del frontend

--- BackEnd/config/allowedOrigins.js
+++ BackEnd/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
    // 'http://localhost:3000',
-   'https://technotes-api-6yrp.onrender.com',
+   //'https://technotes-api-6yrp.onrender.com',
    // 'https://timel.gregdorian.repl.co/',
     'https://technotes-hyq7.onrender.com/'
    
