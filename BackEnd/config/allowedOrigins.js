7169d0a9970928a5666260460a5faebdeb660c5d
gregdorian <greg_dorian@yahoo.com>
2023-12-12 15:08:22 -0500

    add other deployments

--- BackEnd/config/allowedOrigins.js
+++ BackEnd/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
    // 'http://localhost:3000',
     //'http://localhost:5173',
-  //  'https://technotes-opwxjw1jq-gregdorians-projects.vercel.app/',
+    'https://technotes-opwxjw1jq-gregdorians-projects.vercel.app/',
     'https://technotes-hyq7.onrender.com/'
    
 ]
